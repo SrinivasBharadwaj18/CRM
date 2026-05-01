6d9c333fbd3df816c042d7f7368e8ca3b4590042
SrinivasBharadwaj18 <srinivasbharadwaj18@gmail.com>
2026-05-02 01:18:09 +0530

    updated urls.py

--- CRM/CRM/urls.py
+++ CRM/CRM/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('django-backend/', admin.site.urls),
 
     # Include insurer app APIs
     path("api/", include("insurer.urls")),
--- CRM/insurer/urls.py
+++ CRM/insurer/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib import admin
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import (
@@ -28,39 +29,41 @@ from django.views.generic import TemplateView
 urlpatterns = [
 
     # Authentication
-    path("user/login", Login.as_view(), name="login"),
-    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/user/login", Login.as_view(), name="login"),
+    path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+
+    path("api/django-portal/", admin.site.urls),
 
 
     # Admin APIs
-    path("admin/create-user", create_employee, name="create_employee"),
-    path("admin/employees", view_employees, name="view_employees"),
-    path("admin/upload-leads/", upload_leads, name="upload_leads"),
-    path("admin/mega-dashboard/",admin_mega_dashboard, name="admin_dashboard" ),
-    path('admin/current-score/', admin_current_score, name='admin_current_score'),
-    path('admin/agents/', get_all_agents, name='list-agents'), # Need this for the dropdowns
-    path('admin/update-salary/<int:agent_id>/', update_agent_salary, name='update-salary'),
-    path('admin/award-incentive/', admin_award_incentive, name='award-incentive'),
+    path("api/admin/create-user", create_employee, name="create_employee"),
+    path("api/admin/employees", view_employees, name="view_employees"),
+    path("api/admin/upload-leads/", upload_leads, name="upload_leads"),
+    path("api/admin/mega-dashboard/",admin_mega_dashboard, name="admin_dashboard" ),
+    path('api/admin/current-score/', admin_current_score, name='admin_current_score'),
+    path('api/admin/agents/', get_all_agents, name='list-agents'), # Need this for the dropdowns
+    path('api/admin/update-salary/<int:agent_id>/', update_agent_salary, name='update-salary'),
+    path('api/admin/award-incentive/', admin_award_incentive, name='award-incentive'),
 
-    path("agent/leads/",agent_leads, name="agent_leads"),
+    path("api/agent/leads/",agent_leads, name="agent_leads"),
 
-    path("leads/<int:id>/",lead_detail, name="lead_detail"),
+    path("api/leads/<int:id>/",lead_detail, name="lead_detail"),
 
-    path("agent/<int:id>/process/",process_lead, name="process_lead"),
+    path("api/agent/<int:id>/process/",process_lead, name="process_lead"),
 
-    path("agent/<int:id>/await/",await_lead, name="await_lead"),
+    path("api/agent/<int:id>/await/",await_lead, name="await_lead"),
 
-    path("agent/<int:id>/log-call/", log_call, name="log_call"),
+    path("api/agent/<int:id>/log-call/", log_call, name="log_call"),
 
-    path("agent/dashboard-stats/", agent_dashboard_stats, name="agent_dashboard_stats"),
+    path("api/agent/dashboard-stats/", agent_dashboard_stats, name="agent_dashboard_stats"),
 
-    path("agent/check-in/", check_in, name="check_in"),
+    path("api/agent/check-in/", check_in, name="check_in"),
 
-    path("agent/<int:id>/close/",close_lead, name="close_lead" ),
+    path("api/agent/<int:id>/close/",close_lead, name="close_lead" ),
 
-    path("agent/<int:id>/mark-paid/",mark_as_paid, name="mark_paid"),
+    path("api/agent/<int:id>/mark-paid/",mark_as_paid, name="mark_paid"),
 
-    path('agent/earnings-dashboard/', AgentEarningsDashboardView.as_view(), name='agent-earnings'),
+    path('api/agent/earnings-dashboard/', AgentEarningsDashboardView.as_view(), name='agent-earnings'),
 
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 
